1b790628cbaec7709c86e98fe32ab6eb10e1b3a0
cwl <3503127564@qq.com>
2024-10-22 17:36:09 +0800

    部分修改

--- data/dao.php
+++ data/dao.php
@@ -60,16 +60,16 @@ function selectLinks($tagName)
     $db = new SQLite3(dirname(__FILE__)  . '/db/tagNavigation.db');
 
     $sql = "
-        select l.name, l.icon, l.href, l.status, l.tags
+        select l.name, l.icon, l.href, l.tags
         from (select tl.link
               from tag_link tl
               where tl.tag = :tagName
               group by tl.link) tl
-                 left join (select l.name, l.icon, l.href, l.status, GROUP_CONCAT(distinct tl.tag) as tags
+                 left join (select l.name, l.icon, l.href, GROUP_CONCAT(distinct tl.tag) as tags
                             from link l
                                      left join tag_link tl
                                                on tl.link = l.href
-                            group by l.name, l.icon, l.href, l.status) l
+                            group by l.name, l.icon, l.href) l
                            on tl.link = l.href
     ";
 
@@ -84,7 +84,6 @@ function selectLinks($tagName)
             'name' => $row['name'],         // 名称
             'icon' => $row['icon'],         // 图标
             'href' => $row['href'],         // 链接
-            'status' => $row['status'],     // 状态
             'tags' => explode(',', $row['tags'])  // 将标签字符串转换为数组
         ];
     }
